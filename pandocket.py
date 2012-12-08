01a5cf74db31f5a494ebe0edb58f53b3f8097b59
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2012-12-08 12:57:42 -0600

    Added --noimages option

--- pandocket.py
+++ pandocket.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('filename', action='store', help='Name of input file to process')
 parser.add_argument('outname', action='store', help='Basename for output files')
 parser.add_argument('--mdonly', action='store_true', help='Output markdown only, suppress EPUB and PDF output')
+parser.add_argument('--noimages', action='store_true', help='Remove image tags from webpage (recommended for PDF and EPUB output)')
 sysargs = parser.parse_args()
 
 # Open the input file and empty the output file
@@ -26,18 +27,31 @@ for line in input:
 
 		# Get information from line about website to be parsed
 		# Help provided by http://stackoverflow.com/questions/13537829/
-		vars = line.split(' | ')
-		url, args = line.split (' | ', 1)
+		# vars = line.split(' | ')
+		if line.count('|') == 1:
+			url, args = line.split (' | ', 1)
+			user = None
+		elif line.count('|') == 2:
+			url, args, user = line.split (' | ', 2)
 		args = args.split(' >')
-		name = args[0]
+		tag = args[0]
 		params = dict([param.strip().split('=') for param in args[1:]])
 
 		# Open specified URL and use BeautifulSoup to get specified section
 		html = urllib2.urlopen(url).read()
 	 	soup = BeautifulSoup(html)
- 		html_section = str(soup.find(name, **params))
+ 		html_section = soup.find(tag, **params)
+
+		if user is not None:
+			user = __import__(user)	
+			html_section = user.pandocket(html_section)
+
+		if (sysargs.noimages):
+			for image in html_section.find_all(name='img'):
+				image.decompose()
+
 		doc = pandoc.Document()
-		doc.html = html_section
+		doc.html = str(html_section)
 		html_md = doc.markdown
 
  		# Write to output file, getting rid of any literal linebreaks
@@ -53,7 +67,7 @@ for line in input:
 		# Pass regular lines from input file to output file
 		f = open(sysargs.outname + ".md","a").write(line + "\n")
 
-# Call on pandoc to convert fulltext to markdown and write to file
+# Call on pandoc to convert markdown to PDF and EPUB
 # Using yoavram fork of pyandoc
 
 if not (sysargs.mdonly):
